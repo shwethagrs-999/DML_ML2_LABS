d5f686954c9c6c81b4f83bf9286802d22cb0bdb5
Shwetha G R <shwethagrs@gmial.com>
2025-12-05 14:36:20 +0530

    added

---
+++ hello_flow.py
@@ -0,0 +1,14 @@
+from prefect import flow, task
+
+@task
+def say_hello(name: str):
+       print(f"Hello, {name}!")
+
+@flow
+def hello_flow():
+       say_hello("MLOps Student")
+
+if __name__ == "__main__":
+       hello_flow()
+
+
